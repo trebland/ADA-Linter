6047e0735a233aa174d9440f7c6a84cd4aae09f8
Trevor Bland <54162416+trebland@users.noreply.github.com>
2023-07-24 05:20:07 -0400

    Resume type usage from parse5 (#8)

--- src/validators/image.ts
+++ src/validators/image.ts
@@ -1,10 +1,11 @@
 import { TAG_NAMES } from "../enums/tags.js";
+import { Element } from "../../node_modules/parse5/dist/tree-adapters/default.js";
 
 const enum IMAGE_ATTRIBUTES {
   alt = "alt",
 }
 
-export function validateImageAttributes(node: any) {
+export function validateImageAttributes(node: Element) {
   if (node.tagName !== TAG_NAMES.IMG) return;
 
   if (!node.attrs.find((e: any) => e.name === IMAGE_ATTRIBUTES.alt) ?? true) {
